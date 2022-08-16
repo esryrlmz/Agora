e5242b035ca98d0140a84b16b90ee43bd677766f
esryrlmz <esra.yorulmaz@bil.omu.edu.tr>
2022-08-16 15:18:40 +0300

    kullanıcı paneli, product duzenleme tamamlandı

--- Agora.UI/Controllers/ProductController.cs
+++ Agora.UI/Controllers/ProductController.cs
@@ -21,16 +21,18 @@ namespace Agora.UI.Controllers
         IProductRepository _repoProduct;
         IConfiguration _configuration;
         ICommentRepository _repoComment;
+        IUserRepository _repoUser;
         [System.Obsolete]
         private IHostingEnvironment Environment;
 
         [Obsolete]
         public ProductController(ICategoryRepository repoCategory, IProductRepository repoProduct,
-            IConfiguration configuration, IHostingEnvironment environment, ICommentRepository repoComment)
+            IConfiguration configuration, IHostingEnvironment environment, ICommentRepository repoComment, IUserRepository repoUser)
         {
             _repoCategory = repoCategory;   
             _repoProduct = repoProduct;
             _repoComment = repoComment;
+            _repoUser=repoUser;
             _configuration = configuration;
             Environment = environment;
             
@@ -105,6 +107,67 @@ namespace Agora.UI.Controllers
         {
             var subCategoryList = _repoCategory.SubCategoryList(Convert.ToInt32(CategoryId));
             return Json(subCategoryList);
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            Product prd = _repoProduct.GetFullProduct(id);
+            ProductDto productdto = new ProductDto();
+            if (prd.ProductCategories[0].Category.CategoryID != null)
+            { productdto.CategoryID = (int)prd.ProductCategories[0].Category.CategoryID; }
+            else { productdto.CategoryID = prd.ProductCategories[0].Category.ID; }
+            productdto.SubCategoryID = prd.ProductCategories[0].Category.ID;
+            productdto.CategoryName = prd.ProductCategories[0].Category.CategoryName;
+            productdto.ShortName = prd.ShortName;
+            productdto.Description = prd.Description;
+            productdto.IsActive = prd.IsActive;
+            productdto.Pictures = _repoProduct.GetProductImages(id);
+            productdto.ProductID = prd.ID;
+            ViewBag.username = _repoUser.GetUserDetail(prd.UserID).NameSurname;
+            return View((productdto, _repoCategory.GetAllCategory()));
+        }
+        [HttpPost]
+        [System.Obsolete]
+        public IActionResult Edit([Bind(Prefix = "Item1")] ProductDto editPrd)
+        {
+
+            // 1.Adımı: kategoriler değişmiş mi?
+            if (editPrd.SubCategoryID == 0) // ANA KATEGORİYE GEÇTİYSE
+            {
+                _repoProduct.updateProductCategory(editPrd.ProductID, editPrd.CategoryID);
+            }
+            else // ALT KATEGORİYE GEÇTİYSE
+            {
+                _repoProduct.updateProductCategory(editPrd.ProductID, editPrd.SubCategoryID);
+            }
+
+            //2.adım: model verilerini güncelle
+            _repoProduct.updateProduct(new Product()
+            {
+                ID = editPrd.ProductID,
+                ShortName = editPrd.ShortName,
+                Description = editPrd.Description,
+                IsActive = editPrd.IsActive
+            });
+
+            //3.adım: modelin resimleri değişmişmi, değişeni cloudinary sisteminden kaldır
+            //yeni resmi cloudinarye ekle ve oluşan url linkini db den güncelle
+            foreach (ProductPicture picture in editPrd.Pictures)
+            {
+                List<ProductPicture> pictures = new List<ProductPicture>();
+                if (picture.Image != null)
+                {
+                    CloudinaryImage cimage = new CloudinaryImage(_configuration, Environment);
+                    cimage.CloudinaryDestroyImage(picture.PictureUrl);
+                    pictures.Add(picture);
+                    List<string> ImageUrl = cimage.LocalUpload(pictures);
+                    picture.PictureUrl = ImageUrl[0];
+                    _repoProduct.UpdateProductPicture(picture);
+                }
+            }
+
+            return RedirectToAction("ViewProduct", new { id = editPrd.ProductID });
         }
     }
 }
